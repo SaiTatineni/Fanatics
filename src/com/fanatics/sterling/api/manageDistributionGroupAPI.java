a964afe135f38cc79b83c09ce75092f45d73456c
NeerajKumarSharda <neeraj.sharda@lightwellinc.com>
2016-04-13 13:06:33 +0530

    Fanatics Vendor Location Feed
    Committed by Neeraj Sharda. 
    1. More comments were added.
    2. The name of the constants file was changed. So, the code change was incorporated.

--- src/com/fanatics/sterling/api/manageDistributionGroupAPI.java
+++ src/com/fanatics/sterling/api/manageDistributionGroupAPI.java
@@ -1,15 +1,41 @@
+package com.fanatics.sterling.api;
+
 /**
- *  @author Neeraj Sharda
- *  
+ * This class contains the Vendor Location Feed functionality required for the fanatics Etaildirect Implementation.
+ * 
  *  This code is called when the Vendor Location Feed is received by the Fanatics OMS system, to perform the below mentioned functionalities:
  *  1. Remove Vendor from the Distribution Group before the input Vendor Location is deleted from the OMS.
  *  2. Add Vendor to the  Distribution Group before updating the Vendor information, as per the input xml.
  *  
  *  Note: Method "processNodeForMonitoringDG" performs the above mentioned functions
  *  
+ * @(#) manageDistributionGroupAPI.java    
+ * Created on   April 13, 2016
+ *              
+ *
+ * Package Declaration: 
+ * File Name:       manageDistributionGroupAPI.java
+ * Package Name:    com.fanatics.sterling.api;
+ * Project name:    Fanatics
+ * Type Declaration:    
+ * Class Name:      manageDistributionGroupAPI
+ * 
+ * @author Neeraj Sharda
+ * @version 1.0
+ * @history none
+ *     
+ * 
+ *  
+ *
+ * (C) Copyright 2006-2007 by owner.
+ * All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information
+ * of the owner. ("Confidential Information").
+ * Redistribution of the source code or binary form is not permitted
+ * without prior authorization from the owner.
+ *
  */
-
-package com.fanatics.sterling.api;
 
 import java.util.Properties;
 
@@ -17,7 +43,7 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 import com.fanatics.sterling.util.CommonUtil;
-import com.fanatics.sterling.util.FANConstants;
+import com.fanatics.sterling.constants.VendorFeedConstants;
 import com.fanatics.sterling.util.XMLUtil;
 import com.yantra.interop.japi.YIFCustomApi;
 import com.yantra.yfs.japi.YFSEnvironment;
@@ -31,50 +57,50 @@ public class manageDistributionGroupAPI implements YIFCustomApi {
 
 		System.out.println("inXML is "+ XMLUtil.getXMLString(inXML));
 		// get the value of RTAM DG and the Calling Organization Code from the Arguments of the service
-		final String strRTAMDGCode = getPropertyVal(FANConstants.ARG_fanatics_RTAM_DISTRIBUTION_GROUP);
-		final String strCallingOrgCode = getPropertyVal(FANConstants.ARG_fanatics_CALLING_ORG_CODE);
+		final String strRTAMDGCode = getPropertyVal(VendorFeedConstants.ARG_fanatics_RTAM_DISTRIBUTION_GROUP);
+		final String strCallingOrgCode = getPropertyVal(VendorFeedConstants.ARG_fanatics_CALLING_ORG_CODE);
 
 		System.out.println("strRTAMDGCode is "+strRTAMDGCode);
 		System.out.println("strCallingOrgCode is "+strCallingOrgCode);
 
 		// set both the properties in the inXML as DefaultDistributionRuleId and CallingOrganizationCode
 		Element rootInXML = inXML.getDocumentElement();
-		rootInXML.setAttribute(FANConstants.ATT_fanatics_DEFAULT_DISTRIBUTION_RULE_ID, strRTAMDGCode);
-		rootInXML.setAttribute(FANConstants.ARG_fanatics_CALLING_ORG_CODE, strCallingOrgCode);
+		rootInXML.setAttribute(VendorFeedConstants.ATT_fanatics_DEFAULT_DISTRIBUTION_RULE_ID, strRTAMDGCode);
+		rootInXML.setAttribute(VendorFeedConstants.ARG_fanatics_CALLING_ORG_CODE, strCallingOrgCode);
 		System.out.println("inXML is 1"+ XMLUtil.getXMLString(inXML));
 
 		// find out the required action
-		String strOperation = rootInXML.getAttribute(FANConstants.ATT_fanatics_OPERATION);
+		String strOperation = rootInXML.getAttribute(VendorFeedConstants.ATT_fanatics_OPERATION);
 		System.out.println("strOperation is "+ strOperation);
 		/**
 		 *  invoke the service getDistributionList
 		 */
-		docOutXML = CommonUtil.invokeService(env, FANConstants.SERVICE_fanatics_GET_DISTRIBUTION_LIST, inXML);
+		docOutXML = CommonUtil.invokeService(env, VendorFeedConstants.SERVICE_fanatics_GET_DISTRIBUTION_LIST, inXML);
 		System.out.println("docOutXML is 1"+ XMLUtil.getXMLString(docOutXML));
 
 
 		// check if the vendor is a part of DG already
 		Element rootdocOPgetDistributionList = docOutXML.getDocumentElement();
 		System.out.println("first condition " + rootdocOPgetDistributionList.hasChildNodes());
-		System.out.println("sec condition "+strOperation.equalsIgnoreCase(FANConstants.CONSTANT_fanatics_DELETE));
+		System.out.println("sec condition "+strOperation.equalsIgnoreCase(VendorFeedConstants.CONSTANT_fanatics_DELETE));
 
-		if (rootdocOPgetDistributionList.hasChildNodes() && strOperation.equalsIgnoreCase(FANConstants.CONSTANT_fanatics_DELETE)){
+		if (rootdocOPgetDistributionList.hasChildNodes() && strOperation.equalsIgnoreCase(VendorFeedConstants.CONSTANT_fanatics_DELETE)){
 			// if the vendor is a part of DG 
 			/**
 			 *  invoke deleteDistribution
 			 */
 			System.out.println("point 1");
-			docOutXML = CommonUtil.invokeService(env, FANConstants.SERVICE_fanatics_DELETE_DISTRIBUTION, inXML);	
+			docOutXML = CommonUtil.invokeService(env, VendorFeedConstants.SERVICE_fanatics_DELETE_DISTRIBUTION, inXML);	
 		}
 
 		// else if the vendor is not present in the DG and the operation is not Delete: Add the vendor to the DG
 
-		else if ( !rootdocOPgetDistributionList.hasChildNodes() && !strOperation.equalsIgnoreCase(FANConstants.CONSTANT_fanatics_DELETE)){
+		else if ( !rootdocOPgetDistributionList.hasChildNodes() && !strOperation.equalsIgnoreCase(VendorFeedConstants.CONSTANT_fanatics_DELETE)){
 			/**
 			 *  invoke createDistributionList
 			 */
 			System.out.println("point 2");
-			docOutXML = CommonUtil.invokeService(env, FANConstants.SERVICE_fanatics_CREATE_DISTRIBUTION, inXML);
+			docOutXML = CommonUtil.invokeService(env, VendorFeedConstants.SERVICE_fanatics_CREATE_DISTRIBUTION, inXML);
 		}
 
 		return docOutXML;
