43fd29e8bf3fbdf96b67349cb7243ded20974797
jacktyrrell <jacktyrrell@hotmail.co.uk>
2016-04-18 16:04:17 +0100

    Revert "minor changes to FANYFSGetOrderNoUEImpl"
    This reverts commit d11e1b829924fe288f96d699acbf7e6a79f0e8cb.

--- src/com/fanatics/sterling/ue/FANYFSGetOrderNoUEImpl.java
+++ src/com/fanatics/sterling/ue/FANYFSGetOrderNoUEImpl.java
@@ -71,13 +71,11 @@ public class FANYFSGetOrderNoUEImpl implements YFSGetOrderNoUE {
 			seqNoStr   = Long.toString(seqNo);
 
 		} catch (Exception e) {
-			log.error("FANYFSGetOrderNoUEImpl --> ERROR: " + e.getMessage(), e);
 			throw new YFSUserExitException("Error_SEQ_FANATICS_ORDER_NO");
 		}
-		
 		log.verbose("Returning Order no:" + seqNoStr + "for DocumentType::" + documentType);
 
-		log.verbose("Leaving FANYFSGetOrderNoUEImpl -> getOrderNo");
+		log.verbose("Leaving FANYFSGetOrderNoUEImpl->getOrderNo");
 
 		return seqNoStr;
 
